5a4d0f7d9cb2f6f12ff81672ea314cc3997e7421
Shreyasdeep Gayan <140839642+ShreyasGayan@users.noreply.github.com>
2026-06-13 12:34:39 +0530

    Add files via upload

---
+++ Basic-Terminal-Calculator.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cmath>
+
+namespace inp {
+    double x;
+    double y;
+}
+namespace op {
+    char x;
+}
+
+int main() {
+    std::cout << "Enter first number: ";
+    std::cin >> inp::x;
+    
+    std::cout << "Enter Operator: ";
+    std::cin >> op::x;
+    
+    std::cout << "Enter second number: ";
+    std::cin >> inp::y;
+
+
+    switch (op::x)
+    {
+        case '+':
+        std::cout << inp::x + inp ::y;
+        break;
+        
+        case '-' :
+        std::cout << inp::x - inp ::y;
+        break;
+        
+        case '*' :
+        std::cout << inp::x * inp ::y;
+        break;
+        
+        case '/' :
+        std::cout << inp::x / inp ::y;
+        break;
+        
+        case '%' :
+        std::cout << (int)inp::x % (int)inp ::y;
+        break;
+        
+        default: std::cout << "Not a valid operation";
+    }
+        
+    return 0;
+}
+
